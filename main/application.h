5560d6d80f83760debfabee8eeec2012516d5e2f
ElonYang <dolby514@163.com>
2025-09-22 19:02:16 +0800

    上传播放 TF卡ogg音频文件接口，开机新增自动播放 /sdcard/welcome.ogg 文件做辅助测试用

--- main/application.h
+++ main/application.h
@@ -63,6 +63,7 @@ public:
     void SetAecMode(AecMode mode);
     AecMode GetAecMode() const { return aec_mode_; }
     void PlaySound(const std::string_view& sound);
+    void PlaySoundOGGFile(const char *filePath);
     AudioService& GetAudioService() { return audio_service_; }
 
 private:
